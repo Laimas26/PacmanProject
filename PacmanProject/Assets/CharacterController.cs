dc0d3ce66843bbfb671240d4d6d47fbae05d9b83
Laimas26 <laimonas.rupeika@gmail.com>
2022-03-10 18:01:25 +0200

    ez game

--- PacmanProject/Assets/CharacterController.cs
+++ PacmanProject/Assets/CharacterController.cs
@@ -22,9 +22,4 @@ public class CharacterController : MonoBehaviour
             rb.AddForce(Vector3.back);
 
     }
-
-    private void OnTriggerEnter(Collider other){
-    Score.instance.AddPoint();
-    Destroy(gameObject);
-    } 
 }
