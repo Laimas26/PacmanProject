59d8703a3da7d81c94683ab9f49c8a4e4ec50a25
Laimas26 <laimonas.rupeika@gmail.com>
2022-03-10 18:59:01 +0200

    just diff

--- PacmanProject/Assets/CharacterController.cs
+++ PacmanProject/Assets/CharacterController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+
+    public Animator anim;
+    float speed = 1;
+
     void Start()
     {
         
@@ -11,15 +15,24 @@ public class CharacterController : MonoBehaviour
 
     private void Update()
     {
+
         Rigidbody rb = GetComponent<Rigidbody>();
         if (Input.GetKey(KeyCode.A))
             rb.AddForce(Vector3.left);
         if (Input.GetKey(KeyCode.D))
+        {
             rb.AddForce(Vector3.right);
+            anim.SetFloat("speed", speed);
+        }
+            
         if (Input.GetKey(KeyCode.W))
             rb.AddForce(Vector3.forward);
         if (Input.GetKey(KeyCode.S))
             rb.AddForce(Vector3.back);
+
+        //speed = Input.GetAxis("Horizontal");
+
+        //anim.SetFloat("speed", speed);
 
     }
 }
